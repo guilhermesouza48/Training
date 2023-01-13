14150c39d353c1807e0b5423a0de60fecafc0c01
Guilherme Souza de Jesus <guilhermesouzacrs@hotmail.com>
2023-01-12 22:11:52 -0300

    Start: Indentify - Training pt2

---
+++ Indentify/index.js
@@ -0,0 +1,7 @@
+var ano = document.getElementById('ano');
+var sexo = document.getElementById('sexo');
+var botao = document.getElementById('botao');
+var imagens = document.getElementById('imagens');
+var nascimento = document.getElementById('nascimento')
+
+ano.innerHTML += `Ano de Nascimento:`
