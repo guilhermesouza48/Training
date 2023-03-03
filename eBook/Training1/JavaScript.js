860f4904295c4441ee01494f198606057bd11007
Guilherme Souza de Jesus <guilhermesouzacrs@hotmail.com>
2023-03-03 20:22:36 -0300

    eBook : JS DOM

---
+++ eBook/Training1/JavaScript.js
@@ -0,0 +1,15 @@
+/* Document Object Model (DOM) */
+
+
+// document.querySelector("button").addEventListener(
+// "click", () => {
+//     alert("teste JS");
+// });
+
+var testeButton = document.getElementById("testeID");
+
+testeButton.addEventListener(
+    "click", () => {
+        alert("teste");
+        button.style.backgroundColor = "red";
+});
